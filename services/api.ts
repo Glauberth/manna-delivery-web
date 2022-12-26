bcb1d05b305736adeb049b984b888999f66e6e23
Glauberth Almeida Pinho <glauberthalmeidapinho@MacBook-Pro-de-Glauberth.local>
2022-12-26 14:13:17 -0300

    Projeto direcionado para microserviço (API Somento p/ Produtos)

--- services/api.ts
+++ services/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://api.mannatech.com.br/api/v1/",
-  baseURL: "http://localhost:9001/api/v1/",
+  baseURL: "http://64.31.19.104:9001/api/v1/",
   timeout: 15000,
 });
