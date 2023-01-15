1d3ec747a6bc8bdd4e596fedc25e61a4a9d17aef
Glauberth Almeida Pinho <glauberthalmeidapinho@MacBook-Pro-de-Glauberth.local>
2023-01-15 00:30:32 -0300

    Ajustes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f83152ebc7e06e38"; }
+/******/ 		__webpack_require__.h = function() { return "fca77550ffb6144a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- services/api.ts
+++ services/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://api.mannatech.com.br/api/v1/",
-  baseURL: "http://64.31.19.104:9001/api/v1/",
+  baseURL: "http://lgvhost.mannatech.com.br:9001/api/v1/",
   timeout: 15000,
 });
