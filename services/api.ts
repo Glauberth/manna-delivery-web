790aa37b315f4ad032b315c522cf9bc55010c42f
Glauberth Almeida Pinho <glauberthalmeidapinho@MacBook-Pro-de-Glauberth.local>
2023-03-09 13:51:54 -0300

    Inclusão arquivo .env

--- services/api.ts
+++ services/api.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:9001/api/v1/",
+  //baseURL: "http://localhost:9001/api/v1/",
   //baseURL: "http://api.mannatech.com.br/api/v1/",
-  //baseURL: "http://lgvhost.mannatech.com.br:9001/api/v1/",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "http://lgvhost.mannatech.com.br:9001/api/v1/"
+      : "http://localhost:9001/api/v1/",
   timeout: 15000,
 });
