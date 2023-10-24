febd9c0fc5547909efe75f3e921754ee92fcf301
Glauberth Almeida <60085017+Glauberth@users.noreply.github.com>
2023-10-24 12:46:45 -0300

    Adição do nextjs-progressBar para carregamento entre as pags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 import { Provider as AppContextProvider } from "../contexts/app";
 import { Provider as AuthContextProvider } from "../contexts/auth";
 
@@ -7,6 +8,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthContextProvider>
       <AppContextProvider>
+        <NextNProgress
+          color="#5c3f3f"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+        />
         <Component {...pageProps} />
       </AppContextProvider>
     </AuthContextProvider>
