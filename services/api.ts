8b4b909c4e6c43af3d4a701ca99c4ad6bcc6166a
Glauberth Almeida <60085017+Glauberth@users.noreply.github.com>
2024-04-16 15:01:26 -0300

    axios recebe url da api com ssl

--- services/api.ts
+++ services/api.ts
@@ -6,6 +6,6 @@ export const api = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_AMBIENTE == "development"
       ? "http://localhost:9001/api/v1/"
-      : "http://apidelivery.mannatech.com.br:9001/api/v1/", // : "http://lgvhost.mannatech.com.br:9001/api/v1/",
+      : "https://apidelivery.mannatech.com.br/api/v1/", // : "http://lgvhost.mannatech.com.br:9001/api/v1/",
   timeout: 15000,
 });
