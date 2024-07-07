08d5b738ecccf492c6576a98c48347429543f25a
Glauberth Almeida <glauberth.sampaio@hotmail.com>
2024-07-07 16:33:02 -0300

    Ajustes para leitura de qrCode

--- pages/[tenant]/product/[id].tsx
+++ pages/[tenant]/product/[id].tsx
@@ -20,6 +20,31 @@ import ProductImage from "../../../src/components/ProductItem/ProductImagem";
 import Skeleton from "../../../src/components/Skeleton/Skeleton";
 import { IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner";
 
+const formatsBarCodes = [
+  "aztec",
+  "code_128",
+  "code_39",
+  "code_93",
+  "codabar",
+  "databar",
+  "databar_expanded",
+  "data_matrix",
+  "dx_film_edge",
+  "ean_13",
+  "ean_8",
+  "itf",
+  "maxi_code",
+  "micro_qr_code",
+  "pdf417",
+  "qr_code",
+  "rm_qr_code",
+  "upc_a",
+  "upc_e",
+  "linear_codes",
+  "matrix_codes",
+  "unknown",
+];
+
 const Products = (data: Props) => {
   const {
     data: produtoQuery,
