c42699a8e57537c5598c86cd232acf3fad7a1a4c
Glauberth Almeida <glauberth.sampaio@hotmail.com>
2024-08-28 21:58:58 -0300

    feat: Arquivo api.ts agora pega as baseUrl direto do arquivo .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -14,7 +14,7 @@ console.log(`${dataAtual} ${process.env.NEXT_PUBLIC_AMBIENTE}`);
 export const api = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_AMBIENTE == "development"
-      ? "http://localhost:9001/api/v1/"
-      : "https://apidelivery.mannatech.com.br/api/v1/", // : "http://lgvhost.mannatech.com.br:9001/api/v1/",
+      ? process.env.NEXT_PUBLIC_URL_API_DEVELOPMENT
+      : process.env.NEXT_PUBLIC_URL_API_PRODUCTION, // : "http://lgvhost.mannatech.com.br:9001/api/v1/",
   timeout: 15000,
 });
